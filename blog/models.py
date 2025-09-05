7b31b3d61ce7e6f4257c134690efc1f2a7e07063
Theo Ojochegbe <symplytheo@gmail.com>
2025-09-05 19:10:27 +0100

    feat: build and configure models

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,56 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    # slugify name if slug is not passed
+    def save(self, *args, **kwargs):
+        if(not self.slug):
+            self.slug = slugify(self.name)
+
+# Create an "Uncategorized" category instance
+# uncategorized, created = Category.objects.get_or_create(id=1, name='Uncategorized')
+# # sets default category to 1 - Uncategorized when main category is deleted
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+    
+     # slugify post title if slug is not passed
+    def save(self, *args, **kwargs):
+        if(not self.slug):
+            self.slug = slugify(self.title)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=100) # author's name
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+       return f"Comment by {self.author} on {self.post.title}"
+    
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='replies')
+    author = models.CharField(max_length=100) # author's name
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+       return f"Reply by {self.author} to comment - {self.comment.id}"
